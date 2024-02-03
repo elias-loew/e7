abcf436fc425288f1e0a6545a27748cf0d396ca7
elias-loew <125573430+elias-loew@users.noreply.github.com>
2024-02-03 19:51:31 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,51 @@
+basic.showLeds(`
+    # # # . .
+    # . . . .
+    # # # . .
+    # . . . .
+    # # # . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # # # . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    # # # # #
+    # . . . #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # # # .
+    # . . . .
+    . # # # .
+    . . . . #
+    . # # # .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    . . . # .
+    # # # # #
+    . # . . .
+    # . . . .
+    `)
+basic.pause(100)
+basic.forever(function () {
+	
+})
